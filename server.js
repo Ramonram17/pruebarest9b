91ea5c77339e2f9cd0f7b0110eb9c0481a345793
Ramon <2236001074@utna.edu.mx>
2025-06-18 16:51:47 -0600

    primer comit del apirest 9B

---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./app/app')
+const config = require('./app/config/configuracion');
+
+app.listen(config.PORT,()=>{
+    console.log("Aplicación corriendo en puerto ", 
+    config.PORT);
+})
