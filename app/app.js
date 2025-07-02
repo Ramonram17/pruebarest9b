02e94c73c221c1b9a99c5aac04b6aac431955459
Ramon <2236001074@utna.edu.mx>
2025-07-02 17:51:53 -0600

    Se crea el controlador y se realiza el primer endpoint

--- app/app.js
+++ app/app.js
@@ -5,6 +5,6 @@ const router  = require('./routes/gymrouter')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.use('/', router)
+app.use('/gym', router)
 
 module.exports=app;
